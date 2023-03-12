2a2f15f56a3197807b35bae3be5d72fbea68075e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-12 18:50:37 +0700

    show account properties in the account dialog

--- app/src/account/AccountDialog.tsx
+++ app/src/account/AccountDialog.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Close as CloseIcon } from "@mui/icons-material";
 import {
   AppBar,
+  Box,
   Dialog,
   IconButton,
+  TextField,
   Toolbar,
   Typography,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
+import { Account } from "./account";
 
 interface Props {
+  account: Account;
   open: boolean;
   onClose?: () => void;
 }
 
-function AccountDialog({ open, onClose }: Props) {
+function AccountDialog({ account, open, onClose }: Props) {
   const theme = useTheme();
   const downSm = useMediaQuery(theme.breakpoints.down("sm"));
   return (
@@ -31,11 +35,27 @@ function AccountDialog({ open, onClose }: Props) {
           <IconButton onClick={onClose} color="inherit">
             <CloseIcon />
           </IconButton>
-          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+          <Typography sx={{ ml: 2 }} variant="h6" component="div">
             Account Property
           </Typography>
         </Toolbar>
       </AppBar>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", m: 3, mt: 4, gap: 4 }}
+      >
+        <TextField
+          label="Name"
+          value={account.name}
+          inputProps={{ readOnly: true }}
+          fullWidth
+        />
+        <TextField
+          label="Amount"
+          value={account.amount}
+          inputProps={{ readOnly: true }}
+          fullWidth
+        />
+      </Box>
     </Dialog>
   );
 }
--- app/src/account/AccountGrid.tsx
+++ app/src/account/AccountGrid.tsx
@@ -10,11 +10,16 @@ interface Props {
 
 function AccountGrid({ accounts }: Props) {
   const [open, setOpen] = useState(false);
+  const [account, setAccount] = useState({ name: "", amount: 0 });
   const items = [];
   for (const [i, account] of accounts.entries()) {
+    const onClick = () => {
+      setAccount(account);
+      setOpen(true);
+    };
     items.push(
       <Grid key={i} item xs={6} sm={4} md={3}>
-        <AccountCard account={account} onClick={() => setOpen(true)} />
+        <AccountCard account={account} onClick={onClick} />
       </Grid>
     );
   }
@@ -23,7 +28,11 @@ function AccountGrid({ accounts }: Props) {
       <Grid container spacing={2}>
         {items}
       </Grid>
-      <AccountDialog open={open} onClose={() => setOpen(false)} />
+      <AccountDialog
+        account={account}
+        open={open}
+        onClose={() => setOpen(false)}
+      />
     </div>
   );
 }
